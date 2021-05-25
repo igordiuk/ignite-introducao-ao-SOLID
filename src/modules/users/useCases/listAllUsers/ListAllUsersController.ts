318f2645dd6a84e3bfb86e7a40931581e794d60a
Igor Popadiuk <igordiuk@gmail.com>
2021-05-24 22:35:38 -0300

    Exercitando com o Solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (!user_id) {
+        throw Error("User not found!")
+    }
+
+    const all = this.listAllUsersUseCase.execute(user_id);
+
+    return response.status(200).json(all);
   }
 }
 
